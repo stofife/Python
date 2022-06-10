6793e7c54e47e4f5114eeaea686087516301b3fe
stofife <stefandubnicka315@gmail.com>
2022-06-10 18:48:32 +0200

    pp

--- piskvorky.py
+++ piskvorky.py
@@ -1,19 +1,6 @@
-from cupshelpers import Printer
-
-
-grid, player = 5*"######\n", "0"
+grid, player = ["#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"], "0"
 while True:
-    print(grid)
-    print(f"Na rade je hrac {player}\n")
-    x = int(input("Zadaj x-ovu suradnicu: "))
-    y = int(input("Zadaj y-ovu suradnicu: "))
-    grid = grid.split()[x + y*5] = player
-    
-    if player == "X": 
-        if input("Vyhra? (a/n): ") == "a": break
-    
-    if player == "0": 
-        player = "X"
-    else: 
-        player = "0"
+    x,y, grid[x + y*5] = int(input(" 12345\n1" + "".join(grid[0:5]) + "\n2" + "".join(grid[5:10]) + "\n3" + "".join(grid[10:15]) + "\n4" + "".join(grid[15:20]) + "\n5" + "".join(grid[20:25]) + "\n" + f"\nNa rade je hrac {player}\n" + "\nZadaj x-ovu suradnicu: ")), int(input("Zadaj y-ovu suradnicu: ")), player
+    if input("\nVyhra? (a/n): ") == "a": break
+    player = "X" if player == "0" else "0"
 print(f"-------> Vyhral {player} <-------")
