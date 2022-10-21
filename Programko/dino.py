aa54df9f0f40bd7c558058274eebc3a8cafd63f3
(S||T)ofi <63477290+stofife@users.noreply.github.com>
2022-10-21 17:45:40 +0200

    Add files via upload

--- Programko/dino.py
+++ Programko/dino.py
@@ -1,18 +1,60 @@
-import curses
-from curses.textpad import Textbox, rectangle
+import curses, time, random, os, math
 
-def main(stdscr):
-    rectangle(stdscr, 1,0, 1+5+1, 1+30+1)
+from numpy import true_divide
+
+ground = list(3*"__.__.._,_,,,__._,")
+print(len(ground)/3)
+play = ["" for i in range(10)]
+
+cacti = [" ** *", " ****", "***" " **"]
+
+score = 0
+
+# h: 10 lns, w: 18 lns
+
+dino ="""     **** 
+*    *****
+*  ****\\ 
+ ****** & 
+  ****    
+   L L
+"""
+
+dino =["     **** ", "*    *****", "*  ****\\ ", " ****** & ", "  ****", "  L L"]
+
+stdscr = curses.initscr()
+
+stdscr.nodelay(True)
+
+while True:
+    # set framerate
+    time.sleep(1/30)
+    
+    #add score
+    score += 1/10
+    stdscr.addstr(2, 50, str(math.floor(score)))
+    
+    # getting input
+    c = stdscr.getch()
+    if c == ord("q"): 
+        break
+    if c == ord(" "):
+        jump = 3
+    
+    #add obstacles
+    if random.randint(1,1000) == 42:
+        del ground[-5:-1]
+        ground.append(random.choice(["**", "***"]))
+    
+    # draw ground
+    ground.append(ground.pop(0))
+    stdscr.addstr(9, 0, "".join(ground))
+    
+    #draw dino
+    for i in range(6):
+        stdscr.addstr(5+i, 0, dino[i])
+    
+    #refresh
     stdscr.refresh()
 
-
-    c = stdscr.getch()
-    if c == ord(' '):
-        stdscr.addstr(0,0, "Henlo")
-        while True:
-            c = stdscr.getch()
-            if c == ord('q'):
-                break
-            stdscr.refresh()
-
-curses.wrapper(main)
+os.system("clear")
