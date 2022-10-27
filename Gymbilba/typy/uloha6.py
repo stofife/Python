296a0d6d62a70a882b11c117c7b72a82e17d12c1
stofife <stefandubnicka315@gmail.com>
2022-10-27 08:40:32 +0200

    yfthft

---
+++ Gymbilba/typy/uloha6.py
@@ -0,0 +1,3 @@
+#while True: inp += input(": ").split(" ")
+#inp = [[input(": ").split(" ") for _ in range(3)][i][3] for i in range(3)][1:].replace(",", ".") for i in range(3)]), 
+#print(inp)
---
+++ Gymbilba/typy/uloha7.py
@@ -0,0 +1,2 @@
+student = []
+while (student := student + f"{input('Ziak: ')}#{input('Znamky: ')}".split("#")): print(student)#[print(f"{student[i]}") for i in range(len(student))]
