e7223d9c7cf78f0976588d33b318c146bdafd573
stofife <stefandubnicka315@gmail.com>
2022-10-27 07:39:34 +0200

    dawddgdggh

--- Gymbilba/typy/uloha5.py
+++ Gymbilba/typy/uloha5.py
@@ -1,10 +1,7 @@
 
 class Vylet:
     def __init__(self, date, dist, dur, dest) -> None:
-        self.date = date
-        self.dist = dist
-        self.dur = dur
-        self.dest = dest
+        self.date, self.dist, self.dur, self.dest = date, dist, dur, dest
 
 vylety = []
 
@@ -12,12 +9,9 @@ while True:
     inp = input(": ")
     
     if inp != "e":
-        inp = inp.split(";")
+        inp = inp.split("; ")
         vylety.append(Vylet(inp[0], float(inp[1]), int(inp[2]), inp[3]))
     else: break
-    
-def uprav():
-    for vylet in vylety:
-        print(f"{vylet.date} {vylet.dist} {str(vylet.dur / 60).split('.')[0]}:{int(float('0.' + str(vylet.dur / 60).split('.')[1]) * 60)} {round(vylet.dist / (vylet.dur / 60), 1)} {vylet.dest}")
 
-uprav()
+for vylet in vylety:
+    print(f"{vylet.date} {vylet.dist} {str(vylet.dur / 60).split('.')[0]}:{int(float('0.' + str(vylet.dur / 60).split('.')[1]) * 60)} {round(vylet.dist / (vylet.dur / 60), 1)} {vylet.dest}")
